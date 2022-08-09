6bcd289f2ff246793e8f9c4d2c63f2eb06d1be2e
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-08-09 23:47:48 +0200

    par cucc

--- main.js
+++ main.js
@@ -13,20 +13,46 @@ function login() {
 }
 
 window.onload=function(){
-document.getElementById("subscriber").addEventListener("mouseover", mouseOver);
-let subki = [document.getElementById("subki"), document.getElementById("subki2"), document.getElementById("subki3")]
-subki.forEach(cuccok=>{cuccok.addEventListener("mouseover", mouseOut)});
-
-
-
-function mouseOver() {
-  let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
-  menu.forEach(elem =>{elem.style.display = "block"});
+  document.getElementById("subscriber").addEventListener("mouseover", mouseOver);
+  let subki = [document.getElementById("subki"), document.getElementById("subki2"), document.getElementById("subki3")]
+  subki.forEach(cuccok=>{cuccok.addEventListener("mouseover", mouseOut)});
+  
+  
+  
+  function mouseOver() {
+    let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
+    menu.forEach(elem =>{elem.style.display = "block"});
+  }
+  
+  
+  function mouseOut() {
+    let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
+    menu.forEach(elem =>{elem.style.display = "none"});
+  }
+  
+  document.getElementById("ads").addEventListener("mouseover", adsOver);
+  let adski = [document.getElementById("adski"), document.getElementById("adski2"), document.getElementById("adski3")]
+  adski.forEach(cuccok=>{cuccok.addEventListener("mouseover", kiadol)});
+  
+  
+  function adsOver() {
+    let menu = [document.getElementById("ads-menu1"), document.getElementById("ads-menu2"), document.getElementById("ads-menu3")]
+    menu.forEach(elem =>{elem.style.display = "block"});
+  }
+  
+  
+  function kiadol(event) {
+    let menu = [document.getElementById("ads-menu1"), document.getElementById("ads-menu2"), document.getElementById("ads-menu3"), document.getElementById("game-menu1"), document.getElementById("game-menu2"), document.getElementById("game-menu3")]
+    menu.forEach(elem =>{elem.style.display = "none"});
+  }
+  }
+function jelszonezes() {
+  var x = document.getElementById("psw");
+  if (x.type === "password") {
+      x.type = "text";
+  }
+  else {
+      x.type = "password";
+  }
 }
 
-
-function mouseOut() {
-  let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
-  menu.forEach(elem =>{elem.style.display = "none"});
-}
-}
