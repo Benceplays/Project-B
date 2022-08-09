d4ce01949b738cb3e4a10c0094e5bae864431dbe
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-08-09 17:06:41 +0200

    par cucc

---
+++ main.js
@@ -0,0 +1,12 @@
+function registration() {
+    var y = document.getElementById("registerbut");
+    var x = document.getElementById("loginbut");
+      y.style.display = "block";
+      x.style.display = "none";
+  }
+function login() {
+    var c = document.getElementById("loginbut");
+    var v = document.getElementById("registerbut")
+      c.style.display = "block";
+      v.style.display = "none";
+}
