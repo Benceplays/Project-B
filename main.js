7d3ba5ec4d3cea53b73f5534d9c2e619e11dd459
BalintKollar852 <balintkollar852@gmail.com>
2022-08-09 20:30:34 +0200

    a
    a

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function registration() {
     var y = document.getElementById("registerbut");
     var x = document.getElementById("loginbut");
@@ -9,4 +10,23 @@ function login() {
     var v = document.getElementById("registerbut")
       c.style.display = "block";
       v.style.display = "none";
+}
+
+window.onload=function(){
+document.getElementById("subscriber").addEventListener("mouseover", mouseOver);
+let subki = [document.getElementById("subki"), document.getElementById("subki2"), document.getElementById("subki3")]
+subki.forEach(cuccok=>{cuccok.addEventListener("mouseover", mouseOut)});
+
+
+
+function mouseOver() {
+  let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
+  menu.forEach(elem =>{elem.style.display = "block"});
+}
+
+
+function mouseOut() {
+  let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
+  menu.forEach(elem =>{elem.style.display = "none"});
+}
 }
