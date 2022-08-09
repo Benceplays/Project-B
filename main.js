69831c2224ec41f242d251c83b1c5a9e3a0bf6a6
BalintKollar852 <balintkollar852@gmail.com>
2022-08-09 22:27:57 +0200

    asd
    asd

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function login() {
     var v = document.getElementById("registerbut");
       c.style.display = "block";
       v.style.display = "none";
+
 }
 
 window.onload=function(){
@@ -29,4 +30,22 @@ function mouseOut() {
   let menu = [document.getElementById("sub-menu"), document.getElementById("sub-menu2"), document.getElementById("sub-menu3")]
   menu.forEach(elem =>{elem.style.display = "none"});
 }
+
+document.getElementById("ads").addEventListener("mouseover", adsOver);
+let adski = [document.getElementById("adski"), document.getElementById("adski2"), document.getElementById("adski3")]
+adski.forEach(cuccok=>{cuccok.addEventListener("mouseover", kiadol)});
+
+
+function adsOver() {
+  let menu = [document.getElementById("ads-menu1"), document.getElementById("ads-menu2"), document.getElementById("ads-menu3")]
+  menu.forEach(elem =>{elem.style.display = "block"});
 }
+
+
+function kiadol(event) {
+  let menu = [document.getElementById("ads-menu1"), document.getElementById("ads-menu2"), document.getElementById("ads-menu3"), document.getElementById("game-menu1"), document.getElementById("game-menu2"), document.getElementById("game-menu3")]
+  menu.forEach(elem =>{elem.style.display = "none"});
+}
+
+}
+
