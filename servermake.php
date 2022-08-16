c1efb9cc5696214c6fca605abbc2e477f091db98
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-08-16 15:54:22 +0200

    par cucc

---
+++ servermake.php
@@ -0,0 +1,21 @@
+<?php
+	$servername = $_POST['servername'];
+	$serverip = $_POST['serverip'];
+	$serverleiras = $_POST['serverleiras'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','teszt');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+        $stmt = $conn->prepare("insert into szerverek(name, ipcim, leiras) values(?, ?, ?)");
+		$stmt->bind_param("ssi", $servername, $serverip, $serverleiras);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo '<script>alert("Sikeresen létrehozva a hirdetés")</script>';
+		echo "<script>window.location = 'profile/profile.html';</script>";
+		$stmt->close();
+		$conn->close();
+	}
+?>
