d38f0c49a5351953fb284eb0e8ca271411131fd3
BalintKollar852 <balintkollar852@gmail.com>
2022-08-17 11:48:40 +0200

    asd
    asd

--- server/servermake.php
+++ server/servermake.php
@@ -1,21 +1,32 @@
 <?php
+	include '../login.php';
 	$servername = $_POST['servername'];
 	$serverip = $_POST['serverip'];
 	$serverleiras = $_POST['serverleiras'];
+	$boosted = 1;
 
-	// Database connection
 	$conn = new mysqli('localhost','wildemhu_csgo','Kuglifej231','wildemhu_csgo');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-        $stmt = $conn->prepare("insert into szerverek(name, ipcim, leiras) values(?, ?, ?)");
-		$stmt->bind_param("ssi", $servername, $serverip, $serverleiras);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo '<script>alert("Sikeresen létrehozva a hirdetés")</script>';
-		echo "<script>window.location = 'profile/profile.html';</script>";
-		$stmt->close();
-		$conn->close();
+		$sql_szerverlekerdezes = "SELECT * FROM registration WHERE login='$_SESSION[loginvaltozo]'";
+		$result_login=mysqli_query($conn, $sql_szerverlekerdezes);
+		if(mysqli_num_rows($result_login)==1){
+			$stmt = $conn->prepare("insert into servers(servername, ipcim, leiras, playername, boosted) values(?, ?, ?, ?, ?)");
+			$stmt->bind_param("ssssi", $servername, $serverip, $serverleiras, $_SESSION['usernamefirst'], $boosted);
+			$execval = $stmt->execute();
+			echo $execval;
+			echo '<script>alert("Sikeresen létrehozva a hirdetés");</script>';
+			echo "<script>window.location = '../profile/profile.php';</script>";
+			$stmt->close();
+			$conn->close();
+		}
+		else{
+			echo '<script>alert("Nem vagy bejelentkezve");</script>';
+			echo "<script>window.location = '../server/server.php';</script>";
+		}
 	}
+
+
 ?>
