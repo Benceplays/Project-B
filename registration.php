213a21cf094d4a4ac527b3d93fd5a2bc5116dcec
BalintKollar852 <balintkollar852@gmail.com>
2022-08-18 14:54:37 +0200

    nem lehet ugyanolyan felhasználónév/email a regisztrációnál

--- registration.php
+++ registration.php
@@ -11,18 +11,33 @@
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
 		if($email != "" and strpos($email, '@') !== false and $username != "" and $password != "" and $password != "" and $password == $password2){
-		$stmt = $conn->prepare("insert into registration(email, username, password, date, password2) values(?, ?, ?, ?, ?)");
-		$stmt->bind_param("ssssi", $email, $username, $password, $date, $password2);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo '<script>alert("Sikeres regisztráció")</script>';
-		echo "<script>window.location = 'login/logincucc.php';</script>";
-		$stmt->close();
-		$conn->close();
+		$query_all = "SELECT email, username FROM registration";
+    	$result_all = mysqli_query($conn, $query_all);
+    	$adatok_all= mysqli_fetch_assoc($result_all);
+			if($adatok_all['email'] != $email){
+				if($adatok_all['username'] != $username){
+					$stmt = $conn->prepare("insert into registration(email, username, password, date, password2) values(?, ?, ?, ?, ?)");
+					$stmt->bind_param("ssssi", $email, $username, $password, $date, $password2);
+					$execval = $stmt->execute();
+					echo $execval;
+					echo '<script>alert("Sikeres regisztráció!")</script>';
+					echo "<script>window.location = 'login/logincucc.php';</script>";
+					$stmt->close();
+					$conn->close();
+				}
+				else{
+					echo '<script>alert("Sikertelen regisztráció: Ez a felhasználónév már foglalt!")</script>';
+					echo "<script>window.location = 'login/logincucc.php';</script>";
+				}
+			}
+			else{
+				echo '<script>alert("Sikertelen regisztráció: Ezzel az email címmel már regisztráltak!")</script>';
+				echo "<script>window.location = 'login/logincucc.php';</script>";
+			}
 	}
 	else{
-	echo '<script>alert("Sikertelen regisztráció")</script>';
-	echo "<script>window.location = 'index.php';</script>";
+	echo '<script>alert("Sikertelen regisztráció!")</script>';
+	echo "<script>window.location = 'login/logincucc.php';</script>";
 	}
-	}
+}
 ?>
