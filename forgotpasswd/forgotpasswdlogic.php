185c74b5026c0ec8c401ccebd873aa01bec68bed
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-10-07 20:48:35 +0200

    par cucc

--- forgotpasswd/forgotpasswdlogic.php
+++ forgotpasswd/forgotpasswdlogic.php
@@ -2,14 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aktiválás</title>
-    <script src="../main.js"></script>
+    <title>Elfelejtett jelszó</title>
     <link rel="stylesheet" href="forgotpasswdlogic.css">
 </head>
 <body>
-      <form action="forgotpasswdlogic.php" method="post">
+<form action="forgotpasswdlogic.php" method="post">
     <div class="activationdiv">
         <h1  >Elfelejtett jelszó</h1>
         <h3>Aktivációs kódod:</h3>
@@ -18,44 +16,39 @@
         <input class="loginobject" type="password" name="forgotpasswd2" id="forgotpasswd2" placeholder="Új jelszó még egyszer" require><br><br>
         <button name="activationbutton" class="activationbutton">Küldés</button>
     </div>
-    </form>
-    <?php 
+</form>
+</body>
+</html>
+<?php 
         $conn = new mysqli('localhost','wildemhu_csgo','Kuglifej231','wildemhu_csgo');
         if($conn->connect_error){
           echo "$conn->connect_error";
           die("Connection Failed : ". $conn->connect_error);
         }
         else {
-          $email = $_POST['emailaddress'];
-          $activation = random_int(100000, 999999);
-          $uname = "SELECT username FROM registration WHERE email='$email'";
-          $result=mysqli_query($conn, $uname);
-          $adatok = mysqli_fetch_assoc($result);
-          $username = $adatok['username'];
-          $subject = "Jelszó visszaállítás";
-          $body = "Szia $username! Az imént kérelémezted a jelszavad visszaállítását. \nItt találod az aktivációs kódod: $activation";
-          $headers = "From: support.wildemhu@wildem.hu";
-          if(isset($_POST['kuldesgomb'])){
-            mail($email, $subject, $body, $headers);
-            echo "<script>window.location = '../forgotpasswd/forgotpasswdlogic.php';</script>";
-            $valtchange = "UPDATE registration SET randstr='$activation' WHERE username='$username'";
-            mysqli_query($conn, $valtchange);
-          }
-          $subjecttwo = "Sikeres jelszó változtatás";
-          $bodytwo = "Sikeres jelszó változtatás.";
-          $headerstwo = "From: support.wildemhu@wildem.hu";
-          $forgotpasswd = $_POST['forgotpasswd'];
-          $forgotpasswd2 = $_POST['forgotpasswd2'];
-          $encryptedpass = base64_encode($forgotpasswd);
-          $encryptedpass2 = base64_encode($forgotpasswd2);
-
-          if(isset($_POST['activationbutton'])){
-                mail($email, $subjecttwo, $bodytwo, $headerstwo);
-                echo "<script>window.location = '../index.php';</script>";
-                $passwdfel = "UPDATE registration SET password='$encryptedpass' WHERE email='$email'";
-                mysqli_query($conn, $passwdfel);
-          }
+            session_start();
+            $email = $_SESSION["email"];
+            $activation = $_SESSION["activation"];
+            $username = $_SESSION["username"];
+            $beactivation = $_POST['activationcode'];
+            $subjecttwo = "Sikeres jelszó változtatás";
+            $bodytwo = "Sikeres jelszó változtatás.";
+            $headerstwo = "From: support.wildemhu@wildem.hu";
+            $forgotpasswd = $_POST['forgotpasswd'];
+            $forgotpasswd2 = $_POST['forgotpasswd2'];
+            $encryptedpass = base64_encode($forgotpasswd);
+            $encryptedpass2 = base64_encode($forgotpasswd2);
+  
+            if(isset($_POST['activationbutton'])){
+                if($encryptedpass == $encryptedpass2 && $activation == $beactivation){
+                    mail($email, $subjecttwo, $bodytwo, $headerstwo);
+                    echo "<script>window.location = '../index.php';</script>";
+                    $passwdfel = "UPDATE registration SET password='$encryptedpass' WHERE email='$email'";
+                    mysqli_query($conn, $passwdfel);
+                }
+            }
         }
-    ?>
-</body>
-</html>
+
+
+
+?>
