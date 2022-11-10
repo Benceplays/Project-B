867ca1e9d8bbe88c9fcd079a4182d7605cb6b260
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-11-10 20:36:53 +0100

    par cucc

--- speak/speak.php
+++ speak/speak.php
@@ -6,20 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="main.css">
-    <script src="main.js"></script>
-<title>ASD</title>
+    <link rel="stylesheet" href="speak.css">
+    <script src="../main.js"></script>
+<title>Bejelentések</title>
 </head>
 <body>
 
       
-  <ul class="ul">
-    <li class="li" ><a class="li-a" href="index.php">Kezdőlap</a></li>
+<ul class="ul">
+    <li class="li" ><a class="li-a" href="../index.php">Kezdőlap</a></li>
     <li class="li">
-      <ul style="padding: 0;"><a class="li-a" href="elofizetes/elofizetesek.php" id="subscriber">Előfizetések</a></ul>
+      <ul style="padding: 0;"><a class="li-a" href="../elofizetes/elofizetesek.php" id="subscriber">Előfizetések</a></ul>
     </li>
     <li class="li">
-      <ul style="padding: 0;"><a class="li-a" href="report/report.php" id="subscriber">Hiba bejelentés</a></ul>
+      <ul style="padding: 0;"><a class="li-a" href="report.php" id="subscriber">Hiba bejelentés</a></ul>
     </li>
     
     <li class="li" onmouseover="beadol()" onmouseout="kiadol()">
@@ -34,15 +34,15 @@
     }
     </script>
       <ul style="padding: 0;"><a class="li-a" href="#" >Hirdetések</a>
-        <li id="ads-menu2" style="list-style-type: none;display:none; "><a class="li-a" href="servers/servers.php">Keresés</a></li>   
-        <li id="ads-menu1" style="list-style-type: none; display:none"><a class="li-a" href="server/server.php">Készítés</a></li>                         
+        <li id="ads-menu2" style="list-style-type: none;display:none; "><a class="li-a" href="../servers/servers.php">Keresés</a></li>   
+        <li id="ads-menu1" style="list-style-type: none; display:none"><a class="li-a" href="../server/server.php">Készítés</a></li>                         
       </ul>
     </li>
     <li class="li">
       <ul style="padding: 0;"><a class="li-a" href="speak/speak.php" id="subscriber">Beszélgetések</a></ul>
     </li>
     <?php 
-    include 'login.php';
+    include '../login.php';
     $conn = new mysqli('localhost','wildemhu_csgo','Kuglifej231','wildemhu_csgo');
 
     if($conn->connect_error){
@@ -52,16 +52,16 @@
       $loginsql = "SELECT * FROM registration WHERE username='$_SESSION[usernamefirst]' AND login='$_SESSION[loginvaltozo]'";
       $result_login=mysqli_query($conn, $loginsql);
       if(mysqli_num_rows($result_login)==0){
-        echo '<li class="li" style="float: right;" ><a class="li-a" href="login/logincucc.php">Bejelentkezés</a></li>';
+        echo '<li class="li" style="float: right;" ><a class="li-a" href="../login/logincucc.php">Bejelentkezés</a></li>';
       }
       if(mysqli_num_rows($result_login)==1){
         if(isset($_POST['kilepes'])) {
           $logincucosvaltozo = 0;
           $update_login = "UPDATE registration SET login='$logincucosvaltozo' WHERE username='$_SESSION[usernamefirst]' ";
           $resultlogin_update = mysqli_query($conn, $update_login);
-          echo "<script>window.location = 'index.php';</script>";
+          echo "<script>window.location = '../index.php';</script>";
         }
-        echo '<li class="li" style="float: right;" onmouseover="loginpanel()" onmouseout="loginoutpanel()"><ul style="padding: 0;"><a class="li-a" href="#">', $_SESSION["usernamefirst"], '</a><li id="login-menu2" style="list-style-type: none;display:none; "><a class="li-a" href="profile/'.$_SESSION["usernamefirst"].'.php">Profilom</a></li>   
+        echo '<li class="li" style="float: right;" onmouseover="loginpanel()" onmouseout="loginoutpanel()"><ul style="padding: 0;"><a class="li-a" href="#">', $_SESSION["usernamefirst"], '</a><li id="login-menu2" style="list-style-type: none;display:none; "><a class="li-a" href="../profile/profile.php">Profilom</a></li>   
         <li id="login-menu1" style="list-style-type: none; display:none"><form method="post"><input class="button" type="submit" name="kilepes" class="kilepes" value="Kilépés" /></form></li></ul></li>';
         echo '<script>
         function loginpanel() {
@@ -77,13 +77,39 @@
     }
     ?>
   </ul>
-    
-    <div class="" style="padding-top: calc(100vh / 2.8); padding-bottom: 0px;">
-
-        <h2 class="orange-text" style="text-align: center;" >Welcome!</h2>
-        <div class="col d-flex justify-content-center text-white">
-          
+    <div class="chat_main">
+        <div class="chat_messages">
+            <?php print($datas['username']); ?>
+        </div>
+        <form action="" method="post">
+        <div class="chat_input_main">
+            <input type="chat_input" class="chat_input" name="chat_input" id="chat_button">
+            <button class="chat_button" name="chat_button" id="chat_button">Küldés</button>
+        </div>
+        </form>
     </div>
-  </div>
+    <div class="friends_main">
+
+    </div>
+    <?php
+    $conn = new mysqli('localhost','wildemhu_globalchat','Kuglifej231','wildemhu_globalchat');
+
+    if($conn->connect_error){
+        echo "$conn->connect_error";
+        die("Connection Failed : ". $conn->connect_error);
+    } else {
+        $alldata = "SELECT username FROM sessionone";
+        $chatrow = mysqli_query($conn, $alldata);
+        $datas= mysqli_fetch_assoc($chatrow);
+        $message = $_POST['chat_input'];
+        $username = $_SESSION['usernamefirst'];
+        $csakugy = "asdas";
+        if(isset($_POST['chat_button'])) {
+            $stmt = $conn->prepare("insert into sessionone(username, text, valamiremajdjolewsz) values(?, ?, ?)");
+            $stmt->bind_param("ssi", $username, $message, $csakugy);
+            $stmt->close();
+        }
+    }
+  ?>
 </body>
 </html>
